ea1ff31b1ae1bb62be10e28d51fdb99761ea11b6
Leandro <leaxavier2014@gmail.com>
2021-12-25 00:56:36 -0500

    agregando private routes

--- src/components/screens/dashboard/DashboardScreen.js
+++ src/components/screens/dashboard/DashboardScreen.js
@@ -14,7 +14,15 @@ export const DashboardScreen = () => {
         <div>
             <h1>contenido de usuario</h1>
             <div>
-                <h3><span>nombre</span></h3>
+                <h3><span>Foto de perfil</span></h3>
+                <h3><span>nombre y apellido</span></h3>
+            </div>
+            <div>
+                <ul>
+                    <li>imagen uno</li>
+                    <li>imagen dos</li>
+                    <li>imagen tres</li>
+                </ul>
             </div>
 
             <button className='btn' onClick={handleLogout}>
---
+++ src/routers/PrivateRouter.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Redirect, Route } from 'react-router';
+
+export const PrivateRoute = ({
+    isAuthenticated,
+    component: Component,
+    ...rest
+
+}) => {
+
+    localStorage.setItem('lastPast', rest.location.pathname);
+
+    return ( <
+        Route {...rest }
+        component = {
+            (props) => (
+                (isAuthenticated) ?
+                < Component {...props }
+                /> :
+                ( < Redirect to = "/login" / > )
+            )
+        }
+        />
+    )
+}
+
+PrivateRoute.protoTypes = {
+    isAuthenticated: PropTypes.bool.isRequired,
+    component: PropTypes.func.isRequired
+
+}
